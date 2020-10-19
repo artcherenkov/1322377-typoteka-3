5ed96d5fc93ffca8c925d60004787449980a4e07
Артем Черенков <art.cherenkov@gmail.com>
2020-10-19 23:09:33 +0700

    Добавляет команду --server

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -3,11 +3,13 @@
 const help = require(`./help.js`);
 const generate = require(`./generate.js`);
 const version = require(`./version.js`);
+const server = require(`./server.js`);
 
 const Cli = {
   [help.name]: help,
   [generate.name]: generate,
-  [version.name]: version
+  [version.name]: version,
+  [server.name]: server
 };
 
 module.exports = {
