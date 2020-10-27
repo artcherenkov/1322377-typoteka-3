820282a83f80381fe6c3000af47313ecc412f497
Артем Черенков <art.cherenkov@gmail.com>
2020-10-27 19:22:08 +0700

    Регистрирует роуты

--- src/express/index.js
+++ src/express/index.js
@@ -5,4 +5,13 @@ const DEFAULT_PORT = 8080;
 
 const app = express();
 
+const myRoutes = require(`./routes/my-routes`);
+const articlesRoutes = require(`./routes/articles-routes`);
+const mainRoutes = require(`./routes/main-routes`);
+
+app.use(`/`, mainRoutes);
+app.use(`/my`, myRoutes);
+app.use(`/articles`, articlesRoutes);
+
+
 app.listen(DEFAULT_PORT, () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`));
